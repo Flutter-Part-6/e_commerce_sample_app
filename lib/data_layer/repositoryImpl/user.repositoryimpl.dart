4f3495d7291feb00a6f9a898c4f8920755af8fdc
Shyunwook <gdnfl0117@gmail.com>
2023-05-19 18:06:13 +0900

    user repository 작성중

--- lib/data_layer/repositoryImpl/user.repositoryimpl.dart
+++ lib/data_layer/repositoryImpl/user.repositoryimpl.dart
@@ -3,4 +3,12 @@ import 'package:injectable/injectable.dart';
 import 'package:sample_app/domain_layer/repository/user.repository.dart';
 
 @Singleton(as: UserRepository)
-class UserRepositoryImpl implements UserRepository {}
+class UserRepositoryImpl implements UserRepository {
+  @override
+  Future<String> getCustomToken({
+    required String userId,
+    String? email,
+  }) {
+    return Future(() => 'test');
+  }
+}
