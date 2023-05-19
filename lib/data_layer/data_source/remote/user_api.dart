8f96203947aa284c6a9577b44db41f0cdf78b02b
Shyunwook <gdnfl0117@gmail.com>
2023-05-19 18:08:03 +0900

    userapi 주석 처리

--- lib/data_layer/data_source/remote/user_api.dart
+++ lib/data_layer/data_source/remote/user_api.dart
@@ -1,15 +1,15 @@
-import 'package:dio/dio.dart';
-import 'package:retrofit/http.dart';
-
-const String _baseUrl = '';
-
-@RestApi(baseUrl: _baseUrl)
-abstract class UserApi {
-  factory UserApi(Dio dio) = _UserApi;
-
-  @POST('/token')
-  Future<String> getCustomToken({
-    required String userId,
-    String? email,
-  });
-}
+// import 'package:dio/dio.dart';
+// import 'package:retrofit/http.dart';
+//
+// const String _baseUrl = '';
+//
+// @RestApi(baseUrl: _baseUrl)
+// abstract class UserApi {
+//   factory UserApi(Dio dio) = _UserApi;
+//
+//   @POST('/token')
+//   Future<String> getCustomToken({
+//     required String userId,
+//     String? email,
+//   });
+// }
