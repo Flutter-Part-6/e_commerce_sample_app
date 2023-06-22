1f9b11f5d56532bc87452686cd0358013cf9e766
최재웅 <jason967@naver.com>
2023-06-22 10:31:48 +0900

    icon assets 추가

---
+++ lib/theme/app_icons.dart
@@ -0,0 +1,20 @@
+class AppIcons {
+  static String mainLogo = 'assets/svg/main_logo.svg';
+  static String location = 'assets/svg/icon-location.svg';
+  static String cart = 'assets/svg/icon-cart.svg';
+  static String chat = 'assets/svg/icon-chat.svg';
+  static String checkMark = 'assets/svg/checkmark.svg';
+  static String chevronRight = 'assets/svg/icon-chevron_right.svg';
+  static String close = 'assets/svg/icon-close.svg';
+  static String closeCircleFill = 'assets/svg/icon-close_alt_fill.svg';
+  static String add = 'assets/svg/icon-add.svg';
+  static String subtract = 'assets/svg/icon-subtract.svg';
+  static String checkMarkCircle = 'assets/svg/icon-checkmark_circle.svg';
+  static String checkMarkCircleFill =
+      'assets/svg/icon-checkmark_circle_fill.svg';
+  static String chatKakaotalk = 'assets/svg/icon-kakaotalk_simple.svg';
+  static String navHome = 'assets/svg/nav-home.svg';
+  static String navCategory = 'assets/svg/nav-category.svg';
+  static String navSearch = 'assets/svg/nav-search.svg';
+  static String navUser = 'assets/svg/nav-user.svg';
+}
