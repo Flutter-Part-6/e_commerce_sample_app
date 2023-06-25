0ab2fdf9281b95e17910251f118896a03b41b92d
최재웅 <jason967@naver.com>
2023-06-26 08:40:54 +0900

    잘못들어간 샘플 파일 제거

---
+++ lib/data_layer/dto/sample/sample.dto.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sample.dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_SampleDto _$$_SampleDtoFromJson(Map<String, dynamic> json) => _$_SampleDto(
+      aa: json['aa'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$$_SampleDtoToJson(_$_SampleDto instance) =>
+    <String, dynamic>{
+      'aa': instance.aa,
+    };
